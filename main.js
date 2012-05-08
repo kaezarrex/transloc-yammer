56234beeee91fcc0378c6b6072eaae6f84a2cb7d
David Hazinski <david.hazinski@transloc.com>
2012-05-08 09:29:50 -0400

    Making extension auto-update.

---
+++ main.js
@@ -0,0 +1,43 @@
+var JQUERY_URL = 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js',
+    $;
+
+addJQuery(letsJQuery);
+
+// Add jQuery
+function addJQuery(callback) {
+    var script = document.createElement('script');
+
+    script.setAttribute('src', JQUERY_URL);
+
+    script.addEventListener('load', function() {
+        var script = document.createElement('script');
+        script.textContent = '(' + callback.toString() + ')();';
+        document.body.appendChild(script);
+    }, false);
+
+    document.body.appendChild(script);
+}
+
+// All your JQuery code must be inside this function
+function letsJQuery() {
+
+    $('#network-type').remove();
+
+    $('#network-logo')
+        .html('<img src="http://transloc.com/templates/transloc/images/transloc_logo.png">')
+        .css({
+            'border': 'none',
+            'box-shadow': 'none',
+            '-webkit-box-shadow': 'none',
+            '-moz-box-shadow': 'none'
+        });
+
+    $('head').append('              \
+        <style type="text/css">     \
+            .yj-avatar-thumb {      \
+                border-radius: 3px; \
+            }                       \
+        </style>                    \
+    ');
+}
+
--- yammer.user.js
+++ yammer.user.js
@@ -5,45 +5,7 @@
 // @description A Yammer beautifier for TransLoc.
 // ==/UserScript==
 
-var JQUERY_URL = 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js',
-    $;
-
-addJQuery(letsJQuery);
-
-// Add jQuery
-function addJQuery(callback) {
-    var script = document.createElement('script');
-
-    script.setAttribute('src', JQUERY_URL);
-
-    script.addEventListener('load', function() {
-        var script = document.createElement('script');
-        script.textContent = '(' + callback.toString() + ')();';
-        document.body.appendChild(script);
-    }, false);
-
-    document.body.appendChild(script);
-}
-
-// All your JQuery code must be inside this function
-function letsJQuery() {
-
-    $('#network-type').remove();
-
-    $('#network-logo')
-        .html('<img src="http://transloc.com/templates/transloc/images/transloc_logo.png">')
-        .css({
-            'border': 'none',
-            'box-shadow': 'none',
-            '-webkit-box-shadow': 'none',
-            '-moz-box-shadow': 'none'
-        });
-
-    $('head').append('              \
-        <style type="text/css">     \
-            .yj-avatar-thumb {      \
-                border-radius: 3px; \
-            }                       \
-        </style>                    \
-    ');
-}
+var e=document.createElement('script');
+e.setAttribute('language','javascript');
+e.setAttribute('src','https://raw.github.com/kaezarrex/transloc-yammer/master/main.js');
+document.body.appendChild(e);
